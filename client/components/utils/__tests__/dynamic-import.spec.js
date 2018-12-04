028b946cec8ce40f920b551339efd3f9112c668a
Cesar de Barros <cbsorrilha@hotmail.com>
2018-12-04 21:38:11 -0200

    chore: testando corretamente o dynamic-import

--- client/components/utils/__tests__/dynamic-import.spec.js
+++ client/components/utils/__tests__/dynamic-import.spec.js
@@ -1,14 +1,19 @@
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import { dynamicImport } from '../dynamic-import';
 
 describe('Testing the dynamicImport function', () => {
   const Decoy = () => <div id="decoy">decoy</div>;
 
-  it('should render the Decoy component', () => {
-    const Component = dynamicImport(() => Promise.resolve({ default: Decoy }))();
-    const wrapper = mount(Component);
-    console.log(wrapper.debug());
-    expect(true).toBe(true);
+  it('should render the Decoy component', done => {
+    const mockImport = () => Promise.resolve({ default: Decoy });
+    const Component = dynamicImport(mockImport)();
+    const wrapper = shallow(Component);
+    setTimeout(() => {
+      wrapper.update();
+      const containsDecoy = wrapper.containsMatchingElement(<Decoy />);
+      expect(containsDecoy).toBe(true);
+      done();
+    }, 10);
   });
 });
